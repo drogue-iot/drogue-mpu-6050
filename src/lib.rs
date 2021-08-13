4f3558f39ef2ef3a808c6a4c4e39e56e54690415
高庆丰 <feng@hemayun.com>
2021-08-13 12:24:45 +0800

    no message

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,17 @@
 #![no_std]
 
-pub mod sensor;
-pub mod address;
-pub mod error;
-pub mod registers;
 pub mod accel;
-pub mod gyro;
-pub mod fifo;
+pub mod address;
 pub mod clock_source;
 pub mod config;
 mod dmp_firmware;
-mod firmware_loader;
-pub mod quaternion;
-pub mod gravity;
-pub mod yaw_pitch_roll;
+pub mod error;
 pub mod euler;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub mod fifo;
+mod firmware_loader;
+pub mod gravity;
+pub mod gyro;
+pub mod quaternion;
+pub mod registers;
+pub mod sensor;
+pub mod yaw_pitch_roll;
